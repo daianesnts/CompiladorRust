d0d347fe51a302791d3d2d13d6b6968ce822ab2a
Paulo Italo <pauloitalo061@gmail.com>
2026-06-05 14:33:28 -0300

    Mudança no Parser (IfElse, Expressoes e Chamada de Funcoes)

--- parser.py
+++ parser.py
@@ -115,13 +115,16 @@ def p_stm_continue(p):
 
 #IFELSE
 def p_ifr_no_else(p):
+   '''ifr : IF expr block'''
+   p[0] = ('if', p[2], p[3], None)
     
- 
 def p_ifr_else(p):
-    
+   '''ifr : IF expr block ELSE block'''
+   p[0] = ('if', p[2], p[3], 'else', p[5])
  
 def p_ifr_elseif(p):
-    
+   '''ifr : IF expr block ELSE ifr'''
+   p[0] = ('if', p[2], p[3], 'else', p[5])
  
 #DECLARACOES
 def p_decl_let(p):
@@ -144,56 +147,80 @@ def p_typedecl_empty(p):
  
 #EXPRESSOES
 def p_exp_binop(p):
-
+   '''expr : expr PLUS  expr
+   | expr MINUS expr
+   | expr TIMES expr
+   | expr DIVIDE expr
+   | expr EQUALS expr
+   | expr NEQUALS expr
+   | expr LESS expr
+   | expr GRTR expr'''
+   p[0] = ('binop', p[2], p[1], p[3])
  
 def p_exp_attrib(p):
-   
+   '''attrib : ID EQUALS expr'''
+   p[0] = ('attrib', p[1],p[3])
  
 def p_exp_compound(p):
-   
+   '''compound : LBRACE stmts RBRACE'''
+   p[0] = ('compound', p[2])
  
 def p_exp_unary_not(p):
-   
+   '''expr : LOGNOT expr'''
+   p[0] = ('unary not', p[2])
  
 def p_exp_unary_minus(p):
- 
+   '''expr : MINUS expr'''
+   p[0] = ('unary minus', p[2])
  
 def p_exp_paren(p):
-    
+   '''expr : LPAREN expr RPAREN'''
+   p[0] = ('exp paren', p[2])
  
 def p_exp_block_expr(p):
-    
+   '''expr : LBRACE stmts expr RBRACE'''
+   p[0] = ('block expr', p[2], p[3])
  
 def p_exp_block_stmts(p):
-    
- 
+   '''expr : LBRACE stmts RBRACE'''
+   p[0] = ('block stmts', p[2])
+
 def p_exp_call(p):
-   
+   '''expr : ID LPAREN args RPAREN'''
+   p[0] = ('exp call', p[1], p[3])
  
 def p_exp_number(p):
-    
+   '''expr : NUMBER'''
+   p[0] = ('number', p[1])
  
 def p_exp_true(p):
-   
+   '''expr : TRUE'''
+   p[0] = ('boolean', True)
  
 def p_exp_false(p):
-    
+   '''expr : FALSE'''
+   p[0] = ('boolean', False)    
  
 def p_exp_id(p):
-    
+   '''expr : ID'''
+   p[0] = ('id', p[1])
 
 #CHAMADA DE FUNÇÃO
 def p_call_with_params(p):
-    
+   '''expr : ID LPAREN args RPAREN'''
+   p[0] = ('call with params',p[1],p[3])
  
 def p_call_no_params(p):
-    
+   '''expr : ID LPAREN RPAREN'''
+   p[0] = ('call no params', p[1])
  
 def p_params_single(p):
-    
+   '''args : ID'''
+   p[0] = [p[1]]
  
 def p_params_multi(p):
-   
+   '''args : args COMMA ID'''
+   p[0] = p[1] + [p[3]]
 
 
 def p_error(p):
