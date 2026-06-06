a2a39b1701597b39f5e327b9a1fae51bf256b248
bernardonx20-dot <bernardonx20@gmail.com>
2026-06-06 20:54:35 -0300

    Incrementando as funções das traits e declarações

--- parser.py
+++ parser.py
@@ -4,100 +4,116 @@ import lexer
 
 #PROGRAMA
 def p_program_single(p):
- 'program : topdecl'
- p[0] = ('program', [p[1]]))
+    'program : topdecl'
+    p[0] = ('program', [p[1]])
 
 def p_program_multi(p):
- 'program : topdecl program'
- p[0] = ('program', [p[1]] + p[2][1])
- 
+    'program : topdecl program'
+    p[0] = ('program', [p[1]] + p[2][1])
+
 def p_topdecl_func(p):
- 'topdecl : funcdecl'
- p[0] = p[1]
- 
+    'topdecl : funcdecl'
+    p[0] = p[1]
+
 def p_topdecl_struct(p):
- 'topdecl : structdecl'
- p[0] = p[1]
+    'topdecl : structdecl'
+    p[0] = p[1]
 
 def p_topdecl_trait(p):
- 'topdecl : traitdecl'
- p[0] = p[1]
- 
+    'topdecl : traitdecl'
+    p[0] = p[1]
 
-#FUNCOES 
+
+#FUNÇÕES
 def p_funcdecl(p):
- 'funcdecl : signature body'
- p[0] = ('funcdecl', p[1], p[2])
- 
+    'funcdecl : signature body'
+    p[0] = ('funcdecl', p[1], p[2])
+
 def p_signature(p):
- 'signature : FN ID signaturei'
- p[0] = ('signature', p[2], p[3])
- 
+    'signature : FN ID signaturei'
+    p[0] = ('signature', p[2], p[3])
+
 def p_signaturei_p(p):
- 'signaturei : signaturep'
- p[0] = p[1]
+    'signaturei : signaturep'
+    p[0] = p[1]
 
 def p_signaturei_np(p):
- 'signaturei : signaturenp'
- p[0] = p[1]
+    'signaturei : signaturenp'
+    p[0] = p[1]
 
 def p_signaturep_arrow(p):
- 'signaturep : LPAREN sigparams RPAREN ARROW TYPE'
- p[0] = ('signaturep', p[2], p[5])
+    'signaturep : LPAREN sigparams RPAREN ARROW TYPE'
+    p[0] = ('signaturep', p[2], p[5])
 
 def p_signaturep_no_arrow(p):
- 'signaturep : FN ID LPAREN sigparams RPAREN'
- p[0] = ('signaturep', p[4], None)
+    'signaturep : LPAREN sigparams RPAREN'
+    p[0] = ('signaturep', p[2], None)
 
 def p_signaturenp_arrow(p):
- 'signaturenp : LPAREN RPAREN ARROW TYPE'
- p[0] = ('signaturenp', [], p[4])
+    'signaturenp : LPAREN RPAREN ARROW TYPE'
+    p[0] = ('signaturenp', [], p[4])
 
 def p_signaturenp_no_arrow(p):
- 'signaturenp : FN ID LPAREN RPAREN'
- p[0] = ('signaturenp', [], None)
+    'signaturenp : LPAREN RPAREN'
+    p[0] = ('signaturenp', [], None)
 
-def p_sigparams_empty(p):
- 'sigparams : sigparam'
- p[0] = [p[1]]
+def p_sigparams_single(p):
+    'sigparams : sigparam'
+    p[0] = [p[1]]
 
-def p_sigparams_COMMA(p):
- 'sigparams : sigparam COMMA sigparams'
- p[0] = [p[1]] + p[3]
+def p_sigparams_multi(p):
+    'sigparams : sigparam COMMA sigparams'
+    p[0] = [p[1]] + p[3]
 
-def p_sigparam_single(p):
- 'sigparam : ID COLON TYPE'
- p[0] = ('param', p[1], p[3])
+def p_sigparam(p):
+    'sigparam : ID COLON TYPE'
+    p[0] = ('param', p[1], p[3])
+
 def p_body(p):
- 'body : LBRACE stmts RBRACE'
- p[0] = ('body', p[2])
+    'body : LBRACE stmts RBRACE'
+    p[0] = ('body', p[2])
 
 
 #STRUCT E TRAIT 
 def p_structdecl(p):
-    
- 
+    'structdecl : STRUCT ID LBRACE structfields RBRACE'
+    p[0] = ('structdecl', p[2], p[4])
+
+
 def p_structfields_empty(p):
-    
- 
+    'structfields : empty'
+    p[0] = []
+
+
 def p_structfields_single(p):
-    
- 
+    'structfields : structfield'
+    p[0] = [p[1]]
+
+
 def p_structfields_multi(p):
-    
- 
+    'structfields : structfield COMMA structfields'
+    p[0] = [p[1]] + p[3]
+
+
 def p_structfield(p):
- 
+    'structfield : ID COLON TYPE'
+    p[0] = ('structfield', p[1], p[3])
+
 
 def p_traitdecl(p):
-    
- 
+    'traitdecl : TRAIT ID LBRACE traitbody RBRACE'
+    p[0] = ('traitdecl', p[2], p[4])
+
+
 def p_traitbody_empty(p):
-    
- 
+    'traitbody : empty'
+    p[0] = None
+
+
 def p_traitbody(p):
-    
- 
+    'traitbody : signature SEMICOLON traitbody'
+    p[0] = [p[1]] + p[3]  
+
 
 #COMANDOS 
 def p_stmts_single(p):
@@ -156,23 +172,32 @@ def p_ifr_elseif(p):
    p[0] = ('if', p[2], p[3], 'else', p[5])
  
 #DECLARACOES
+def p_decl(p):
+    'decl : decllet | declmut | declcons | declexp'
+    p[0] = p[1]
 def p_decl_let(p):
-    
+ 'decllet : LET ID typedecl EQUALS exp SEMICOLON'
+ p[0] = ('decllet', p[2], p[3], p[5])
  
 def p_decl_mut(p):
-    
+ 'declmut : LET MUT ID typedecl EQUALS exp SEMICOLON'
+ p[0] = ('declmut', p[3], p[4],p[6])
  
 def p_decl_const(p):
-    
+ 'declcons : CONST ID EQUALS TYPE EQUALS NUM SEMICOLON'
+ p[0] = ('declcons', p[2],p[4], p[6])
 
 def p_decl_exp(p):
-    
+ 'declexp : ID EQUALS exp SEMICOLON'
+ p[0] = ('declexp', p[1], p[3])
  
 def p_typedecl_typed(p):
+ 'typedecl : COLON TYPE'
+ p[0] = ('typedecl', p[2])
     
- 
 def p_typedecl_empty(p):
-    
+ 'typedecl : empty'
+ p[0] = None
  
 #EXPRESSOES
 def p_exp_binop(p):
