43ed2149622afa4172f02fcd172021328d23914f
Pedro Henrique <peedrohs.mesquita@outlook.com>
2026-06-21 20:39:29 -0300

    Exemplo de código rust para testes

--- main.rs
+++ main.rs
@@ -3,6 +3,14 @@ Esta é a função main de um programa em rust.
 
 OBS: O léxico ainda não está completo.
 */
+struct User {
+	username: String,
+	email: String,
+	sign_in_count: u64,
+	active: bool
+}
+
+
 fn main() {
 	let x = 67;	// Aqui é uma variável não mutável
 	let mut y = 42; // Aqui é variável mutável
@@ -10,5 +18,14 @@ fn main() {
 	while y < x && x >= y {
 		y = y + 1;
 	}
+	call();
+	test += 10;
+	test -= 10;
+	test *= 10;
+	test /= 10;
 	/* fim do programa */
+}
+
+trait Describable {
+	fn describe(&self, name : i32) -> String;
 }
