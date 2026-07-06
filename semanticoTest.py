69f5a6c2cae2cbe2c60a550a5f908a453d562100
daianesnts <daidai.santos@hotmail.com>
2026-07-05 23:35:38 -0300

    Estrutura inicial do semântico e tabela de símbolos
    Subindo a base do visitor semântico e a tabela de símbolos. Os métodos do visitor que não implementei foram criados com 'pass' para que outros integrantes continuem o trabalho. Estou também subindo o teste do semântico e um gitignore que crie para facilitar.

---
+++ semanticoTest.py
@@ -0,0 +1,22 @@
+from parser import *
+from visitorSemantico import VisitorSemantico
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Use: python semanticoTest.py <arquivo.rs>")
+        exit(1)
+
+    with open(sys.argv[1], 'r') as f:
+        source_code = f.read()
+    the_lexer = create_lexer()
+    the_parser = create_parser()
+
+    result = the_parser.parse(source_code, lexer=the_lexer)
+
+    visitor = VisitorSemantico()
+    result.accept(visitor)
+
+
+if __name__ == '__main__':
+    main()
