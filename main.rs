e6975187ff519a8dc0396dd9ae5dda40871a38c3
Pedro Henrique <peedrohs.mesquita@outlook.com>
2026-07-29 07:39:06 -0300

    Exemplo de código rust para o 'compilador.py'

--- main.rs
+++ main.rs
@@ -1,41 +1,17 @@
-/*
-Esta é a função main de um programa em rust.
-
-OBS: O léxico ainda não está completo.
-*/
-struct User {
-	username: String,
-	email: String,
-	sign_in_count: u64,
-	active: bool
-}
-
+static X: i32 = 31415;
 
 fn main() {
-	let x = 67;	// Aqui é uma variável não mutável
-	let mut y = 42; // Aqui é variável mutável
-	
-	while y < x && x >= y {
-		y = y + 1;
-		if y > 10 {
-			x += 10;
-		} else if j < 20 {
-			y += 20;
-		} else {
-			j = j + 25 * x + 20;
-		}
+	let mut a = 0;
+	let x = 1;
+	while a <= 5 {
+		let x = 6;
+		a = soma(a, x);
 	}
-	call();
-	test += 10;
-	test -= 10;
-	test *= 10;
-	test /= 10;
-	/* fim do programa */
+	a = a - X;
+	
+	println("Print final");
 }
 
-trait Describable {
-	fn describe(name : i32) -> String;
-	fn describe(nome: i32);
-	fn describe(&self) -> String;
-	fn descbrie(&self);
+fn soma(a: i32, b: i32) -> i32 {
+	return a + b;
 }
